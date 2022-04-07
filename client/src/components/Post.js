c97fbc12c6c561f1a41c3894d8e0abc0998a8b10
Nell Flores <nell.djmf@gmail.com>
2022-04-07 04:01:41 -0400

    formatted for readability

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -19,7 +19,7 @@ const mapDispatchToProps = (dispatch) => {
 
 const Posts = (props) => {
 
-  const [popup, showPopup] = useState(false)
+  const [info, showInfo] = useState(false)
   const [clicked, isClicked] = useState(false)
 
   useEffect(() => {
@@ -38,15 +38,15 @@ const Posts = (props) => {
   return (
     <div className='wrapper'>
       <h1>Travel Blog</h1>
-      <button className='toggle' onClick={()=>showPopup(false)}>Picture</button>
-      <button className='toggle' onClick={()=>showPopup(true)}>Info</button>
+      <button className='toggle' onClick={()=>showInfo(false)}>Picture</button>
+      <button className='toggle' onClick={()=>showInfo(true)}>Info</button>
         {props.postsState.posts && props.postsState.posts.map((post) => (
           <ul key={post._id} className='location-container'>
             <div className='location'>
               <h2 className='location-name' >{post.locationName}</h2>
               <br/>
               {
-              popup ? (
+              info ? (
                 <div className='info-container'>
                     <div style={{
                     maxHeight: "200px",
@@ -57,14 +57,34 @@ const Posts = (props) => {
                   </div>
               ) : (
                 <div>
-                  <img className='location-image' src={post.image} alt={post.location} style={{width: "300px", maxHeight: "180px"}}/>
+                  <img className='location-image' 
+                  src={post.image} 
+                  alt={post.location} 
+                  style={{
+                    width: "300px", 
+                    maxHeight: "180px"}}/>
                 </div>
-              )
+                )
               }
-              <form onSubmit={()=>isClicked(true)} target="ifrm1" id="new-comment" action={`http://localhost:3001/api/${post._id}`} method="POST">
-                <input className='user' type="text" name="username" placeholder="Username"/>
-                <input type="text" name="text" placeholder="Comment Here"/>
-                <input className='rating' type="number" name="review" placeholder="1-5" min="1" max="5"/>
+              <form 
+              onSubmit={()=>isClicked(true)} target="ifrm1" 
+              id="new-comment" 
+              action={`http://localhost:3001/api/${post._id}`} 
+              method="POST">
+                <input 
+                className='user' 
+                type="text" 
+                name="username" 
+                placeholder="Username"/>
+                <input 
+                type="text" 
+                name="text" 
+                placeholder="Comment Here"/>
+                <input 
+                className='rating' 
+                type="number" 
+                name="review" 
+                placeholder="1-5" min="1" max="5"/>
                 <div className='button-wrapper'>
                   <button type="submit">Submit</button>
                   <button type="reset">Clear</button>
@@ -72,11 +92,13 @@ const Posts = (props) => {
               </form>
               <iframe id="ifrm1" name="ifrm1" style={{display: "none"}}></iframe>
             </div>
-            <div className='comment-container' style={{overflowY: "scroll"}} >
+            <div className='comment-container' 
+            style={{overflowY: "scroll"}} >
               <h2>Comments</h2>
               <div className='comments'>
                 {post.comment.map((commentItem) => (
-                  <ul className='comment-item' key={commentItem._id}>
+                  <ul className='comment-item' 
+                  key={commentItem._id}>
                     <h3>{commentItem.username}: {commentItem.review}/5</h3>
                     <p>{commentItem.text}</p>
                   </ul>
